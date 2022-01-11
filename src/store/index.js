589e19c65650e2c802f0d74ffc61947c05e456e2
liuji <1154483502@qq.com>
2022-01-11 17:30:56 +0800

    初始化项目文件

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
 import { createStore } from 'vuex'
-
 export default createStore({
   state: {
+    username: 'zs'
+  },
+  getters: {
+    updateName (state) {
+      return state.username + '....'
+    }
   },
   mutations: {
   },
